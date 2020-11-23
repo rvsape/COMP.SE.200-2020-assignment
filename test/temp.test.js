e742c8e698f9c31e06fbd86474bada299cd9114c
saarabot <saara.pekkala@gmail.com>
2020-11-23 17:13:21 +0200

    Packages: chai & mocha, init tests

---
+++ test/temp.test.js
@@ -0,0 +1,32 @@
+// Temp file to test mocha and chai packages
+var chai = require('chai')
+var assert = chai.assert
+
+describe("Top level test block", () => {
+
+	before(()=> {
+	})
+
+	after(()=> {	
+	})
+
+	beforeEach(()=> {
+	})
+
+	afterEach(()=> {
+	})
+
+    //Example from mocjajs.org
+    describe('Array', function() {
+        describe('#indexOf()', function() {
+            it('should return -1 when the value is not present', function() {
+                assert.strictEqual([1, 2, 3].indexOf(4), -1);
+            });
+        });
+    });
+    
+    it("should return true", ()=>   {
+
+	})
+
+})
