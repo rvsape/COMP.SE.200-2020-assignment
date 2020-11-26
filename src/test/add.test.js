d3ef532b403ef70ebbacc977eb3d60b2f82bbeeb
Masi <sima.mironova@protonmail.com>
2020-11-26 12:41:13 +0200

    add.test.js is created

---
+++ src/test/add.test.js
@@ -0,0 +1,16 @@
+import chai from "chai"
+import add from "../add.js"
+const expect = chai.expect
+
+
+
+describe("Sum", () => {
+    it("adds positive number to positive number", () =>{
+        expect(add(3,6)).to.equal(9)
+    });
+   
+   
+   
+   
+   })
+   
