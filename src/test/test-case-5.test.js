ce0487a4289c495d9401f805334c26e1560284f1
Masi <sima.mironova@protonmail.com>
2020-11-30 15:43:26 +0200

    test#5 is done

---
+++ src/test/test-case-5.test.js
@@ -0,0 +1,66 @@
+import chai from "chai"
+import capitalize from "../capitalize.js"
+import upperFirst from "../upperFirst.js"
+import toString from "../toString.js"
+import isSymbol from "../isSymbol.js"
+const expect = chai.expect
+
+
+
+describe("Test case #5", () => {
+    describe("upperFirst.js", () => {
+        it("should convert the first character of a string to upper case", () =>{
+            expect(upperFirst("milk")).to.equal("Milk")
+        });
+
+        it("should convert the first character of a sring to upper case, test case all upper cases", () =>{
+            expect(upperFirst("MILK")).to.equal("MILK")
+        });
+
+    })
+       
+    describe("isSymbol.js", () => {
+        it("should return `true` when `value` is a symbol", () =>{
+            expect(isSymbol(Symbol.iterator)).to.equal(true)
+        });
+        
+        it("should return `false` when `value` is not a symbol", () =>{
+            expect(isSymbol("123")).to.equal(false)
+        });
+        
+    })
+
+
+
+    describe("toString.js", () => {
+        it("should convert the provided array to string", () =>{
+            expect(toString([1,2,3])).to.equal("1,2,3")
+        });
+
+        it("should return '' for an empty string", () =>{
+            expect(toString(null)).to.equal("")
+        });
+
+        it("should preserve the sign of `-0`", () =>{
+            expect(toString(-0)).to.equal("-0")
+        });
+
+        it("should preserve the string value when value is string", () =>{
+            expect(toString("milk")).to.equal("milk")
+        });
+    })
+    
+    describe("capitalize.js", () => {
+        it("should converts the first character of `string` to upper case and the remaining is keeped with lower case", () =>{
+            expect(capitalize("milk")).to.equal("Milk")
+        });
+
+        it("should converts the first character of `string` to upper case and the remaining with lower case", () =>{
+            expect(capitalize("MILK")).to.equal("Milk")
+        });
+
+        
+    })
+    
+})
+   
