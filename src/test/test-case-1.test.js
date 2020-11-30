944aca2a83050a9b611617102f62d549e30286a4
rvsape <74910065+rvsape@users.noreply.github.com>
2020-11-30 19:37:00 +0200

    Update test-case-1.test.js
    Merge conflicts resolved

--- src/test/test-case-1.test.js
+++ src/test/test-case-1.test.js
@@ -3,18 +3,6 @@ import add from "../add.js"
 const expect = chai.expect
 
 
-<<<<<<< HEAD
-
-describe("Add", () => {
-    it("adds positive number to positive number", () =>{
-        expect(add(3,6)).to.equal(9)
-    });
-   
-   
-   
-   
-   })
-=======
 describe('Test case #1', () => {
     describe("add.js", () => {
         it("adds positive number to positive number", () =>{
@@ -23,5 +11,4 @@ describe('Test case #1', () => {
     
     })  
 })
->>>>>>> be8fbfb64521ce87d173e0df0ebe24d69aabbda4
    
